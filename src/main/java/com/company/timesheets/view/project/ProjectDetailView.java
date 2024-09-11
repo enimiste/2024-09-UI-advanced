464da85cb651ecf691bad17e4c06aff16674ab5c
vashenkoda <d.vaschenko@haulmont.net>
2024-09-11 10:50:30 +0300

    Day four is finished

--- src/main/java/com/company/timesheets/view/project/ProjectDetailView.java
+++ src/main/java/com/company/timesheets/view/project/ProjectDetailView.java
@@ -1,9 +1,22 @@
 package com.company.timesheets.view.project;
 
 import com.company.timesheets.entity.Project;
+import com.company.timesheets.entity.ProjectParticipant;
+import com.company.timesheets.entity.Task;
 import com.company.timesheets.view.main.MainView;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
+import io.jmix.flowui.DialogWindows;
+import io.jmix.flowui.Notifications;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.component.tabsheet.JmixTabSheet;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.kit.action.BaseAction;
+import io.jmix.flowui.model.CollectionContainer;
+import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "projects/:id", layout = MainView.class)
 @ViewController("ts_Project.detail")
@@ -11,4 +24,153 @@ import io.jmix.flowui.view.*;
 @EditedEntityContainer("projectDc")
 @DialogMode(width = "64em")
 public class ProjectDetailView extends StandardDetailView<Project> {
+
+    @ViewComponent
+    private VerticalLayout tasksWrapper;
+    @Autowired
+    private DataManager dataManager;
+    @Autowired
+    private DialogWindows dialogWindows;
+
+    private DataGrid<Task> tasksDataGrid;
+    private DataGrid<ProjectParticipant> participantsDataGrid;
+    @Autowired
+    private Notifications notifications;
+    @ViewComponent
+    private CollectionContainer<Task> tasksDc;
+    @ViewComponent
+    private CollectionContainer<ProjectParticipant> participantsDc;
+    @ViewComponent
+    private CollectionLoader<Task> tasksDl;
+    @ViewComponent
+    private CollectionLoader<ProjectParticipant> projectParticipantsDl;
+
+    public void onTasksDataGridCreate(final ActionPerformedEvent event) {
+        Task newTask = dataManager.create(Task.class);
+        newTask.setProject(getEditedEntity());
+
+        dialogWindows.detail(tasksDataGrid)
+                .newEntity(newTask)
+                .withParentDataContext(getViewData().getDataContext())
+                .open();
+    }
+
+    public void onTasksDataGridEdit(final ActionPerformedEvent event) {
+        Task selectedItem = tasksDataGrid.getSingleSelectedItem();
+        if (selectedItem == null) {
+            return;
+        }
+
+        dialogWindows.detail(tasksDataGrid)
+                .editEntity(selectedItem)
+                .withParentDataContext(getViewData().getDataContext())
+                .open();
+    }
+
+    public void onParticipantsDataGridCreate(final ActionPerformedEvent event) {
+        ProjectParticipant newParticipant = dataManager.create(ProjectParticipant.class);
+        newParticipant.setProject(getEditedEntity());
+
+        dialogWindows.detail(participantsDataGrid)
+                .newEntity(newParticipant)
+                .withParentDataContext(getViewData().getDataContext())
+                .open();
+    }
+
+    public void onParticipantsDataGridEdit(final ActionPerformedEvent event) {
+        ProjectParticipant selected = participantsDataGrid.getSingleSelectedItem();
+        if(selected == null) {
+            return;
+        }
+
+        dialogWindows.detail(participantsDataGrid)
+                .editEntity(selected)
+                .withParentDataContext(getViewData().getDataContext())
+                .open();
+
+    }
+
+    @Subscribe("tabSheet")
+    public void onTabSheetSelectedChange(final JmixTabSheet.SelectedChangeEvent event) {
+        if ("tasksTab".equals(event.getSelectedTab().getId().orElse(""))) {
+            initTasks();
+        }
+
+        if ("participantsTab".equals(event.getSelectedTab().getId().orElse(""))) {
+            initParticipants();
+        }
+    }
+
+    private void initTasks() {
+        if (tasksDataGrid != null) {
+            // It means that we've already opened this tab and initialized table and loader
+            return;
+        }
+
+        tasksDl.setParameter("project", getEditedEntity());
+        tasksDl.load();
+
+        tasksDataGrid = (DataGrid<Task>) getContent().findComponent("tasksDataGrid").get();
+        BaseAction createAction = (BaseAction) tasksDataGrid.getAction("create");
+        createAction.addActionPerformedListener(this::onTasksDataGridCreate);
+        BaseAction editAction = (BaseAction) tasksDataGrid.getAction("edit");
+        editAction.addActionPerformedListener(this::onTasksDataGridEdit);
+    }
+
+    private void initParticipants() {
+        if (participantsDataGrid != null) {
+            // It means that we've already opened this tab and initialized table and loader
+            return;
+        }
+
+        projectParticipantsDl.setParameter("project", getEditedEntity());
+        projectParticipantsDl.load();
+
+        participantsDataGrid =(DataGrid<ProjectParticipant>) getContent().findComponent("participantsDataGrid").get();
+        BaseAction createAction = (BaseAction) participantsDataGrid.getAction("create");
+        createAction.addActionPerformedListener(this::onParticipantsDataGridCreate);
+        BaseAction editAction = (BaseAction) participantsDataGrid.getAction("remove");
+        editAction.addActionPerformedListener(this::onParticipantsDataGridEdit);
+
+    }
+
+
+    @Subscribe
+    public void onBeforeShow(final BeforeShowEvent event) {
+        notifications.show("tasksDc items:" + tasksDc.getItems().size());
+        notifications.show("participantsDc items:" + participantsDc.getItems().size());
+    }
+
+
+    @Subscribe(id = "tasksDc", target = Target.DATA_CONTAINER)
+    public void onTasksDcCollectionChange(final CollectionContainer.CollectionChangeEvent<Task> event) {
+        notifications.show("[tasksDc] CollectionChangeEvent", event.getChangeType() + "");
+    }
+
+    @Subscribe(id = "participantsDc", target = Target.DATA_CONTAINER)
+    public void onParticipantsDcCollectionChange(final CollectionContainer.CollectionChangeEvent<ProjectParticipant> event) {
+        notifications.show("[participantsDc] CollectionChangeEvent", event.getChangeType() + "");
+    }
+
+//    @Install(to = "tasksDataGrid.create", subject = "initializer")
+//    private void tasksDataGridCreateInitializer(final Task task) {
+//        task.setProject(getEditedEntity());
+//    }
+
+//    @Subscribe
+//    public void onInitEntity(final InitEntityEvent<Project> event) {
+//        tasksWrapper.setEnabled(false);
+//    }
+//
+//    @Subscribe
+//    public void onAfterSave(final AfterSaveEvent event) {
+//        tasksWrapper.setEnabled(true);
+//    }
+    
+    
+    
+    
+    
+    
+    
 }
--- src/main/java/com/company/timesheets/view/timeentry/TimeEntryDetailView.java
+++ src/main/java/com/company/timesheets/view/timeentry/TimeEntryDetailView.java
@@ -1,15 +1,21 @@
 package com.company.timesheets.view.timeentry;
 
+import com.company.timesheets.app.TaskSupport;
 import com.company.timesheets.entity.Task;
 import com.company.timesheets.entity.TimeEntry;
 import com.company.timesheets.entity.TimeEntryStatus;
 import com.company.timesheets.entity.User;
 import com.company.timesheets.view.main.MainView;
 import com.company.timesheets.view.task.TaskLookupView;
+import com.vaadin.flow.component.AbstractField;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.data.provider.Query;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.usersubstitution.CurrentUserSubstitution;
 import io.jmix.flowui.DialogWindows;
 import io.jmix.flowui.component.combobox.EntityComboBox;
+import io.jmix.flowui.component.select.JmixSelect;
 import io.jmix.flowui.component.textarea.JmixTextArea;
 import io.jmix.flowui.component.valuepicker.EntityPicker;
 import io.jmix.flowui.kit.action.ActionPerformedEvent;
@@ -19,6 +25,7 @@ import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 @Route(value = "time-entries/:id", layout = MainView.class)
 @ViewController("ts_TimeEntry.detail")
@@ -29,20 +36,22 @@ public class TimeEntryDetailView extends StandardDetailView<TimeEntry> {
     
     public static final String PARAMETER_OWN_TIME_ENTRY = "ownTimeEntry";
     private boolean ownTimeEntry = false;
-    
+
+    @ViewComponent
+    private JmixSelect<TimeEntryStatus> statusField;
     @Autowired
     private CurrentUserSubstitution currentUserSubstitution;
     @ViewComponent
     private JmixTextArea rejectionReasonField;
     @ViewComponent
-    private CollectionLoader<Task> tasksDl;
-    @ViewComponent
     private EntityComboBox<Task> taskField;
     @Autowired
     private DialogWindows dialogWindows;
     @ViewComponent
-    private EntityPicker<User> userField;
-    
+    private EntityComboBox<User> userField;
+    @Autowired
+    private TaskSupport taskSupport;
+
     public void setOwnTimeEntry (boolean ownTimeEntry) {
         this.ownTimeEntry = ownTimeEntry;
     }
@@ -64,7 +73,6 @@ public class TimeEntryDetailView extends StandardDetailView<TimeEntry> {
     @Subscribe(id = "timeEntryDc", target = Target.DATA_CONTAINER)
     public void onTimeEntryDcItemChange(final InstanceContainer.ItemChangeEvent<TimeEntry> event) {
         updateRejectionReasonField();
-        loadTasks();
     }
 
     @Subscribe(id = "timeEntryDc", target = Target.DATA_CONTAINER)
@@ -74,18 +82,12 @@ public class TimeEntryDetailView extends StandardDetailView<TimeEntry> {
         }
         if ("user".equals(event.getProperty())) {
             taskField.setReadOnly(getEditedEntity().getUser() == null);
-            loadTasks();
+            taskField.getDataProvider().refreshAll();
         }
         if ("task".equals(event.getProperty()) && ! ownTimeEntry) {
             userField.setReadOnly(getEditedEntity().getTask() != null);
         }
 
-    }
-
-    private void loadTasks() {
-        User user = getEditedEntity().getUser();
-        tasksDl.setParameter("username", user != null ? user.getUsername() : null);
-        tasksDl.load();
     }
 
     private void updateRejectionReasonField() {
@@ -127,5 +129,48 @@ public class TimeEntryDetailView extends StandardDetailView<TimeEntry> {
         dialogWindow.open();
     }
 
+    @Install(to = "taskField", subject = "itemsFetchCallback")
+    private Stream<Task> taskFieldItemsFetchCallback(final Query<Task, String> query) {
+        User user = getEditedEntity().getUser();
+        String filter = query.getFilter().orElse(null);
+
+        return user != null
+                ? taskSupport.getUserActiveTasks(user, query.getOffset(), query.getLimit(), filter)
+                : taskSupport.getActiveTasks(query.getOffset(), query.getLimit(), filter);
+    }
+
+    @Subscribe
+    public void onReady(final ReadyEvent event) {
+        updateStatusFieldIcon();
+    }
+
+    @Subscribe("statusField")
+    public void onStatusFieldComponentValueChange(final AbstractField.ComponentValueChangeEvent<JmixSelect<TimeEntryStatus>, Object> event) {
+        updateStatusFieldIcon();
+    }
+
+    private void updateStatusFieldIcon() {
+        TimeEntryStatus status = statusField.getValue();
+        Icon icon = status == null ? null : switch (status) {
+            case NEW -> {
+                Icon newIcon = VaadinIcon.PLUS_CIRCLE_O.create();
+                newIcon.setColor("var(--lumo-primary-color)");
+                yield newIcon;
+            }
+            case APPROVED -> {
+                Icon approvedIcon = VaadinIcon.CHECK_CIRCLE_O.create();
+                approvedIcon.setColor("var(--lumo-success-color)");
+                yield approvedIcon;
+            }
+            case REJECTED -> {
+                Icon rejectedIcon = VaadinIcon.EXCLAMATION_CIRCLE_O.create();
+                rejectedIcon.setColor("var(--lumo-error-color)");
+                yield rejectedIcon;
+            }
+            case CLOSED -> VaadinIcon.CLOSE_CIRCLE_O.create();
+        };
+
+        statusField.setPrefixComponent(icon);
+    }
 
 }
